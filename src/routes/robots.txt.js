3cf8bf7c16532340e22865b7ccb26c20417ffb5c
AM <michael.rybintsev@gmail.com>
2020-10-12 14:38:04 +0100

    do things properly

--- src/routes/robots.txt.js
+++
@@ -1,9 +0,0 @@
-const robots = [`UserAgent: '*'`, `Disallow: '/wp-login.php'`, `Disallow: '/*/wp-includes/*'`, `Disallow: '/ads.txt`];
-
-export function get(req, res) {
-	res.setHeader('Content-Type', 'text/plain');
-
-	const content = robots.join('\n');
-
-	res.end(content);
-}
